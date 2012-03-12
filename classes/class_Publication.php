8c64333da7c84722c46cde8e8766f90ee8225d36
chapeyroux <chapeyroux.sebastien@gmail.com>
2012-03-12 18:55:35 +0100

    modification de l'affichage pour le cas ou aucun groupe de cours et/ou aucun groupe d'etudiants n'est lies a la promotion selectionnee

--- classes/class_Publication.php
+++ classes/class_Publication.php
@@ -28,52 +28,58 @@
 		}
 		
 		public function liste_publication() {
-			$liste_groupeCours = Groupe_Cours::liste_groupeCours();
-			$nbre_groupeCours = Groupe_Cours::getNbreGroupeCours();
-			$liste_groupeEtudiants = Groupe_Etudiants::liste_groupeEtudiants();
-			$nbre_groupeEtudiants = Groupe_Etudiants::getNbreGroupeEtudiants();
+			$idPromotion = $_GET['idPromotion'];
+			$liste_groupeCours = Groupe_Cours::liste_groupeCours($idPromotion);
+			$nbre_groupeCours = Groupe_Cours::getNbreGroupeCours($idPromotion);
+			$liste_groupeEtudiants = Groupe_Etudiants::liste_groupeEtudiants($idPromotion);
+			$nbre_groupeEtudiants = Groupe_Etudiants::getNbreGroupeEtudiants($idPromotion);
 			$tab="";
 			
 			echo "$tab<h1>Gestion de la publication</h1>\n";
-			echo "$tab<table class=\"listeCours\">\n";
-			
-			echo "$tab\t<tr class=\"fondGrisFonce\">\n";
-			echo "$tab\t\t<th class=\"fondBlanc\" colspan='2' rowspan='2'></th>\n";
-			echo "$tab\t\t<th colspan='{$nbre_groupeEtudiants}'>Nom des groupes d'étudiants</th>\n";
-			echo "$tab\t</tr>\n";
-			
-			echo "$tab\t<tr class=\"fondGrisFonce\">\n";
-			foreach($liste_groupeEtudiants as $idGroupeEtudiants){
-				$Groupe_Etudiants = new Groupe_Etudiants($idGroupeEtudiants);
-				echo "$tab\t\t<td>".$Groupe_Etudiants->getNom()."</td>\n";
+			if ( ($nbre_groupeCours == 0) || ($nbre_groupeEtudiants == 0) ) {
+				echo "$tab<h2>Aucun groupe de cours et/ou aucun groupe d'étudiants n'a été crées pour cette promotion</h2>\n";
 			}
-			echo "$tab\t</tr>\n";
-			
-			echo "$tab\t<tr>\n";
-			echo "$tab\t<th class=\"fondGrisFonce\" rowspan='{$nbre_groupeCours}'>Nom des groupes <br/>de cours</th>\n";
-			$cpt = 0;
-			foreach($liste_groupeCours as $idGroupeCours){
-				if($cpt == 0){ $couleurFond="fondBlanc"; }
-				else{ $couleurFond="fondGris"; }
-				$cpt++; $cpt %= 2;
+			else {
+				echo "$tab<table class=\"listeCours\">\n";
 				
-				$Groupe_Cours = new Groupe_Cours($idGroupeCours);
-				echo "$tab\t\t<td class=\"fondGrisFonce\">".$Groupe_Cours->getNom()."</td>\n";
+				echo "$tab\t<tr class=\"fondGrisFonce\">\n";
+				echo "$tab\t\t<th class=\"fondBlanc\" colspan='2' rowspan='2'></th>\n";
+				echo "$tab\t\t<th colspan='{$nbre_groupeEtudiants}'>Nom des groupes d'étudiants</th>\n";
+				echo "$tab\t</tr>\n";
+				
+				echo "$tab\t<tr class=\"fondGrisFonce\">\n";
 				foreach($liste_groupeEtudiants as $idGroupeEtudiants){
 					$Groupe_Etudiants = new Groupe_Etudiants($idGroupeEtudiants);
-
-					$nom_case = "case_".$idGroupeCours."_".$idGroupeEtudiants;
-					if (Publication::appartenance_publication ($idGroupeCours, $idGroupeEtudiants))
-						$checked = "checked = \"checked\"" ;
-					else
-						$checked = "";
-							
-					echo "$tab\t\t<td class=\"$couleurFond\"><input type=\"checkbox\" name= \"{$nom_case}\" value=\"{$nom_case}\" onclick=\"appartenance_groupeCours_groupeEtudiants({$idGroupeCours},{$idGroupeEtudiants},this)\" style=\"cursor:pointer;\" {$checked}></td>\n";
-					
+					echo "$tab\t\t<td>".$Groupe_Etudiants->getNom()."</td>\n";
 				}
 				echo "$tab\t</tr>\n";
+				
+				echo "$tab\t<tr>\n";
+				echo "$tab\t<th class=\"fondGrisFonce\" rowspan='{$nbre_groupeCours}'>Nom des groupes <br/>de cours</th>\n";
+				$cpt = 0;
+				foreach($liste_groupeCours as $idGroupeCours){
+					if($cpt == 0){ $couleurFond="fondBlanc"; }
+					else{ $couleurFond="fondGris"; }
+					$cpt++; $cpt %= 2;
+					
+					$Groupe_Cours = new Groupe_Cours($idGroupeCours);
+					echo "$tab\t\t<td class=\"fondGrisFonce\">".$Groupe_Cours->getNom()."</td>\n";
+					foreach($liste_groupeEtudiants as $idGroupeEtudiants){
+						$Groupe_Etudiants = new Groupe_Etudiants($idGroupeEtudiants);
+
+						$nom_case = "case_".$idGroupeCours."_".$idGroupeEtudiants;
+						if (Publication::appartenance_publication ($idGroupeCours, $idGroupeEtudiants))
+							$checked = "checked = \"checked\"" ;
+						else
+							$checked = "";
+								
+						echo "$tab\t\t<td class=\"$couleurFond\"><input type=\"checkbox\" name= \"{$nom_case}\" value=\"{$nom_case}\" onclick=\"appartenance_groupeCours_groupeEtudiants({$idGroupeCours},{$idGroupeEtudiants},this)\" style=\"cursor:pointer;\" {$checked}></td>\n";
+						
+					}
+					echo "$tab\t</tr>\n";
+				}
+				echo "$tab</table>\n";
 			}
-			echo "$tab</table>\n";
 		}
 		
 		public function appartenance_publication ($idGroupe_Cours, $idGroupe_Etudiants) {
