480302ae9c3b167369b6eeef50c9837a5ef36143
Crixus <jeremy.curny@gmail.com>
2012-03-19 20:08:33 +0100

    Modif SQL suppression Style

--- sql/AllCreates.sql
+++ sql/AllCreates.sql
@@ -205,14 +205,6 @@ CREATE TABLE IF NOT EXISTS `Salle` (
 INSERT INTO `Salle` (`id`, `nom`, `nomBatiment`, `capacite`) VALUES
 (0, 'Non assigné', 'DEFAULT', 999);
 
-CREATE TABLE IF NOT EXISTS `Style` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nomCouleur` varchar(50) NOT NULL,
-  `couleurTexte` varchar(50) NOT NULL,
-  `couleurFond` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
-
 CREATE TABLE IF NOT EXISTS `Type_Cours` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(20) NOT NULL,
