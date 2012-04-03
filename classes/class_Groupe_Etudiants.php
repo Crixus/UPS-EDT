eff9d402e994dbab25dba0c27fda39121a08da5b
chapeyroux <chapeyroux.sebastien@gmail.com>
2012-04-03 21:45:00 +0200

    #24 correction htmlentities page groupe etudiants

--- classes/class_Groupe_Etudiants.php
+++ classes/class_Groupe_Etudiants.php
@@ -264,7 +264,7 @@
 			global $messages_notifications, $messages_erreurs;
 			if (isset($_POST['validerAjoutGroupeEtudiants']) || isset($_POST['validerModificationGroupeEtudiants'])){
 				// Vérification des champs
-				$nom = htmlentities($_POST['nom']);
+				$nom = htmlentities($_POST['nom'],ENT_QUOTES,'UTF-8');
 				$nom_correct = PregMatch::est_nom($nom);
 				
 				$Promotion = new Promotion($_GET['idPromotion']);
